aaa2d35c9b2d014ade11704c10fcb7b343189453
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-06 12:02:43 +0300

    first master commit

---
+++ Lab6.3(it).cpp
@@ -0,0 +1,27 @@
+﻿// lab6.3(it).cpp
+#include <iostream>
+#include <iomanip>
+using namespace std;
+void Input(int* k, const int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		cout << "k[" << i << "] = ";
+		cin >> k[i];
+	}
+	cout << endl;
+}
+void Print(int* k, const int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << setw(4) << k[i];
+	cout << endl;
+}
+int main()
+{
+	const int n = 6;
+	int k[n];
+	Input(k, n);
+	Print(k, n);
+	return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include <sstream>
+#include "../lab6.3(it).cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestInput)
+		{
+			const int size = 6;
+			int k[size];
+			istringstream input("10 20 30 40 50 60");
+			cin.rdbuf(input.rdbuf());
+			Input(k, size);
+			Assert::AreEqual(10, k[0]);
+			Assert::AreEqual(20, k[1]);
+			Assert::AreEqual(30, k[2]);
+			Assert::AreEqual(40, k[3]);
+			Assert::AreEqual(50, k[4]);
+			Assert::AreEqual(60, k[5]);
+		}
+		TEST_METHOD(TestPrint)
+		{
+			const int size = 6;
+			int k[size] = { 10, 20, 30, 40, 50, 60 };
+			ostringstream output;
+			cout.rdbuf(output.rdbuf());
+			Print(k, size);
+			Assert::AreEqual(string("  10  20  30  40  50  60\n"), output.str());
+		}
+	};
+}
